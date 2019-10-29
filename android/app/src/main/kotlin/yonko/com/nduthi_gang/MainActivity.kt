3ffa9ea5619b6d396108b8619257c9826ce97921
kilasi <joseph.kilasi@incentro.com>
2019-10-29 12:46:10 +0300

    initial commit

---
+++ android/app/src/main/kotlin/yonko/com/nduthi_gang/MainActivity.kt
@@ -0,0 +1,12 @@
+package yonko.com.nduthi_gang
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
