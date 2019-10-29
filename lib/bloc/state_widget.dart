d3a4721c1cd68ce10fc1d9c0e7d088501e044fb3
kilasi <joseph.kilasi@incentro.com>
2019-10-29 12:53:33 +0300

    set up provider and bloc pattern

---
+++ lib/bloc/state_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nduthi_gang/objects/state.dart';
+import 'package:provider/provider.dart';
+
+class StateWidget extends StatefulWidget {
+  final StateModel state;
+  final Widget child;
+
+  StateWidget({@required this.child, this.state});
+
+  static _StateWidgetState of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(_StateDataWidget)
+            as _StateDataWidget)
+        .data;
+  }
+
+  @override
+  State<StatefulWidget> createState() => new _StateWidgetState();
+}
+
+class _StateWidgetState extends State<StateWidget> {
+  StateModel state;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    if (widget.state != null) {
+      state = widget.state;
+    } else {
+      state = new StateModel();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<StateModel>(
+      builder: (BuildContext context) => StateModel(),
+      child: new _StateDataWidget(
+        data: this,
+        child: widget.child,
+      ),
+    );
+  }
+}
+
+class _StateDataWidget extends InheritedWidget {
+  final _StateWidgetState data;
+
+  _StateDataWidget({
+    Key key,
+    @required Widget child,
+    @required this.data,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_StateDataWidget oldWidget) => true;
+}
---
+++ lib/objects/state.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class StateModel with ChangeNotifier {}
